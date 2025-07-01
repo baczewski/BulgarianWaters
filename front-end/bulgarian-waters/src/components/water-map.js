4aa7d6a02d268df4e90b7be77966ddcf0222bc53
1nsaneYMD <trafficker.shadia@gmail.com>
2025-07-02 00:26:57 +0300

    Display hardcoded markers

--- front-end/bulgarian-waters/src/components/water-map.js
+++ front-end/bulgarian-waters/src/components/water-map.js
@@ -1,7 +1,24 @@
 import * as React from 'react';
-import Map, {NavigationControl} from 'react-map-gl/mapbox';
+import Map, { NavigationControl, Marker } from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const waterResources = [
+    {
+      coordinates: { latitude: 45.221944444, longitude: 29.743333333 },
+      id: 'http://www.wikidata.org/entity/Q1653',
+      name: 'Дунав',
+    },
+    {
+      coordinates: { latitude: 42.25008, longitude: 27.58922 },
+      id: 'http://www.wikidata.org/entity/Q1563774',
+      name: 'Ясна поляна',
+    },
+    {
+      coordinates: { latitude: 42.5, longitude: 27.4 },
+      id: 'http://www.wikidata.org/entity/Q1014783',
+      name: 'Бургаско езеро',
+    },
+];
 
 function WaterMap() {
     return (
@@ -16,6 +33,17 @@ function WaterMap() {
             mapStyle='mapbox://styles/mapbox/streets-v12'
         >
             <NavigationControl position='top-right' />
+
+            {waterResources.map((resource, index) => (
+                <Marker
+                    key={index}
+                    longitude={resource.coordinates.longitude}
+                    latitude={resource.coordinates.latitude}
+                    anchor='bottom'
+                >
+
+                </Marker>
+            ))}
 
         </Map>
     );
